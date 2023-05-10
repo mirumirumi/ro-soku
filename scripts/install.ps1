b0c8c5b1828797eb55c07dc2e8c5797ab571d960
mirumi <mail@mirumi.tech>
2023-05-10 11:30:37 +0900

    ci(release): allow for versions already exist

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -2,15 +2,12 @@ $roSokuURL = "https://github.com/mirumirumi/ro-soku/releases/latest/download/ro-
 $roSokuFolder = "$env:USERPROFILE\AppData\Local\ro-soku"
 $roSokuPath = Join-Path -Path $roSokuFolder -ChildPath "ro-soku.exe"
 
-# Create the ro-soku folder if it doesn't exist
 if (!(Test-Path -Path $roSokuFolder)) {
     New-Item -ItemType Directory -Path $roSokuFolder
 }
 
-# Download ro-soku.exe
 Invoke-WebRequest -Uri $roSokuURL -OutFile $roSokuPath
 
-# Update the PATH environment variable
 $oldPath = (Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment' -Name 'PATH').PATH
 $newPath = "$roSokuFolder;$oldPath"
 
